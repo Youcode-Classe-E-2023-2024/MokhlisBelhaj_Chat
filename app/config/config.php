f7a75dcb22fd6a58e35857dccbfc0e1d651c354a
BelhajMokhlis <belhajmokhlis@gmail.com>
2023-12-27 12:03:21 +0100

    DataBase class

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+
+
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/MokhlisBelhaj_chat/');
+  // Site Name
+  define('SITENAME', 'chat-app');
---
+++ app/libraries/DataBase.php
@@ -0,0 +1,104 @@
+<?php
+class DataBase
+{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = SITENAME;
+    private $dbh;
+    private $error;
+    private $stmt;
+    public function __construct()
+    {
+        $dsn = 'mysql:host=' . $this->host;
+        $options = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+        }
+        if (!$this->databaseExists()) {
+            $this->createDatabase();
+        }
+    }
+
+    private function databaseExists()
+    {
+        $this->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = :databaseName");
+        $this->bind(':databaseName', $this->dbname);
+        $result = $this->single();
+
+        return !empty($result);
+    }
+    private function createDatabase()
+    {
+        $this->query("CREATE DATABASE $this->dbname");
+        $this->execute();
+    }
+    public function query($query)
+    {
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    // Bind values
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+
+    public function execute()
+    {
+
+        return $this->stmt->execute();
+    }
+
+
+    public function resultset()
+    {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+    public function array()
+    {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+    public function single()
+    {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
+
+
+    public function lastInsertId()
+    {
+        return $this->dbh->lastInsertId();
+    }
+}
