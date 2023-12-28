5b4f677b544ac1384639af43e6c9f11dd9c50015
BelhajMokhlis <belhajmokhlis@gmail.com>
2023-12-28 23:59:15 +0100

    fom send

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -31,7 +31,7 @@
                 <div class="flex flex-row items-center justify-between text-xs mt-6">
                     <span class="font-bold">Room</span>
                     <span id="controom" class="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full"></span>
-                  
+
 
 
                     <!-- Modal toggle -->
@@ -83,7 +83,7 @@
                                                 </button>
                                             </div>
                                             <div class="absolute max-h-40 w-full z-10 mt-2 bg-white border border-gray-300 rounded-md shadow-lg overflow-y-auto hidden " id="dropdownContent">
-                                            <label><input class="chekk" type="checkbox" name="selected_users[]" multiple value="<?php echo $_SESSION['user_id']; ?>" class="mr-2" checked disabled hidden></label>
+                                                <label><input class="chekk" type="checkbox" name="selected_users[]" multiple value="<?php echo $_SESSION['user_id']; ?>" class="mr-2" checked disabled hidden></label>
                                                 <?php
                                                 if (!empty($data['users'])) {
                                                     foreach ($data['users'] as $user) {
@@ -100,70 +100,70 @@
                                             </div>
                                         </div>
                                         <!-- js multiselect -->
-                                        
-                        <script>
-                            const searchInput = document.getElementById('searchInput');
-                            const dropdownContent = document.getElementById('dropdownContent');
-                            const userCheckboxes = document.querySelectorAll('input[name="selected_users[]"]');
-                            // const checkboxes = document.querySelectorAll('chekk');
-                            console.log(userCheckboxes);
+
+                                        <script>
+                                            const searchInput = document.getElementById('searchInput');
+                                            const dropdownContent = document.getElementById('dropdownContent');
+                                            const userCheckboxes = document.querySelectorAll('input[name="selected_users[]"]');
+                                            // const checkboxes = document.querySelectorAll('chekk');
+                                            console.log(userCheckboxes);
 
 
-                            var downArrow = document.getElementById('downArrow');
-                            var upArrow = document.getElementById('upArrow');
+                                            var downArrow = document.getElementById('downArrow');
+                                            var upArrow = document.getElementById('upArrow');
 
 
-                            searchInput.addEventListener('input', function() {
-                                filterOptions();
-                                if (searchInput.value.trim() !== '') {
-                                    showUserDropdown()
-                                }
-                            });
+                                            searchInput.addEventListener('input', function() {
+                                                filterOptions();
+                                                if (searchInput.value.trim() !== '') {
+                                                    showUserDropdown()
+                                                }
+                                            });
 
-                            userCheckboxes.forEach(checkbox => {
-                                checkbox.addEventListener('change', updateSelection);
-                            });
+                                            userCheckboxes.forEach(checkbox => {
+                                                checkbox.addEventListener('change', updateSelection);
+                                            });
 
-                            function filterOptions() {
-                                const searchTerm = searchInput.value.toLowerCase();
+                                            function filterOptions() {
+                                                const searchTerm = searchInput.value.toLowerCase();
 
-                                Array.from(dropdownContent.children).forEach(option => {
-                                    const optionText = option.textContent.toLowerCase();
-                                    option.style.display = optionText.includes(searchTerm) ? 'block' : 'none';
-                                });
-                            }
+                                                Array.from(dropdownContent.children).forEach(option => {
+                                                    const optionText = option.textContent.toLowerCase();
+                                                    option.style.display = optionText.includes(searchTerm) ? 'block' : 'none';
+                                                });
+                                            }
 
-                            function updateSelection() {
-                                const selectedOptions = Array.from(userCheckboxes)
-                                    .filter(checkbox => checkbox.checked)
-                                    .map(checkbox => checkbox.value);
+                                            function updateSelection() {
+                                                const selectedOptions = Array.from(userCheckboxes)
+                                                    .filter(checkbox => checkbox.checked)
+                                                    .map(checkbox => checkbox.value);
 
 
-                                console.log('Selected Options:', selectedOptions);
-                            }
+                                                console.log('Selected Options:', selectedOptions);
+                                            }
 
-                            function toggleDropdown() {
-                                var dropdown = document.getElementById('dropdownContent');
-                                dropdown.classList.toggle('hidden');
-                                downArrow.toggleAttribute('hidden');
-                                upArrow.toggleAttribute('hidden');
-                               
+                                            function toggleDropdown() {
+                                                var dropdown = document.getElementById('dropdownContent');
+                                                dropdown.classList.toggle('hidden');
+                                                downArrow.toggleAttribute('hidden');
+                                                upArrow.toggleAttribute('hidden');
 
-                            }
 
-                            function toggle() {
-                                var dropdown = document.getElementById('dropdown');
-                                dropdown.classList.toggle('hidden');
-                                
-                            }
+                                            }
 
-                            function showUserDropdown() {
-                                var Dropdown = document.getElementById('dropdownContent');
-                                Dropdown.classList.remove('hidden');
-                                upArrow.removeAttribute('hidden');
-                                downArrow.setAttribute('hidden', 'true');
-                            }
-                        </script>
+                                            function toggle() {
+                                                var dropdown = document.getElementById('dropdown');
+                                                dropdown.classList.toggle('hidden');
+
+                                            }
+
+                                            function showUserDropdown() {
+                                                var Dropdown = document.getElementById('dropdownContent');
+                                                Dropdown.classList.remove('hidden');
+                                                upArrow.removeAttribute('hidden');
+                                                downArrow.setAttribute('hidden', 'true');
+                                            }
+                                        </script>
                                         <!-- end -->
 
 
@@ -187,118 +187,29 @@
                     <!-- end modal -->
                 </div>
                 <div id='Room' class="flex flex-col space-y-1 mt-4 -mx-2">
-                    
+
                 </div>
             </div>
         </div>
         <div class="flex flex-col flex-auto h-full p-6">
             <div class="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
                 <div class="flex flex-col h-full overflow-x-auto mb-4">
-                    <div  class="flex flex-col h-full">
-                        <div id="chat" class="grid grid-cols-12 gap-y-2">
-                            <div class="col-start-1 col-end-8 p-3 rounded-lg">
-                                <div class="flex flex-row items-center">
-                                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                                        A
-                                    </div>
-                                    <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                                        <div>Hey How are you today?</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-start-1 col-end-8 p-3 rounded-lg">
-                                <div class="flex flex-row items-center">
-                                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                                        A
-                                    </div>
-                                    <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                                        <div>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit. Vel ipsa commodi illum saepe numquam maxime
-                                            asperiores voluptate sit, minima perspiciatis.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-start-6 col-end-13 p-3 rounded-lg">
-                                <div class="flex items-center justify-start flex-row-reverse">
-                                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                                        A
-                                    </div>
-                                    <div class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                                        <div>I'm ok what about you?</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-start-6 col-end-13 p-3 rounded-lg">
-                                <div class="flex items-center justify-start flex-row-reverse">
-                                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                                        A
-                                    </div>
-                                    <div class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                                        <div>
-                                            Lorem ipsum dolor sit, amet consectetur adipisicing. ?
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-start-1 col-end-8 p-3 rounded-lg">
-                                <div class="flex flex-row items-center">
-                                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                                        A
-                                    </div>
-                                    <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                                        <div>Lorem ipsum dolor sit amet !</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-start-6 col-end-13 p-3 rounded-lg">
-                                <div class="flex items-center justify-start flex-row-reverse">
-                                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                                        A
-                                    </div>
-                                    <div class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                                        <div>
-                                            Lorem ipsum dolor sit, amet consectetur adipisicing. ?
-                                        </div>
-                                        <div class="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500">
-                                            Seen
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-start-1 col-end-8 p-3 rounded-lg">
-                                <div class="flex flex-row items-center">
-                                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                                        A
-                                    </div>
-                                    <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                                        <div>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                            Perspiciatis, in.
-                                        </div>
-                                    </div>
-                                </div>
+                    <div class="flex flex-col h-full">
+                        <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                            <div id="chat" class="grid grid-cols-12 gap-y-2">
+
                             </div>
 
                         </div>
                     </div>
                 </div>
-                <div class="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
-                    <div>
-                        <button class="flex items-center justify-center text-gray-400 hover:text-gray-600">
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
-                            </svg>
-                        </button>
-                    </div>
+                <!-- <div class="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
+
                     <div class="flex-grow ml-4">
                         <div class="relative w-full">
                             <input type="text" class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10" />
                             <button class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
-                                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                </svg>
+
                             </button>
                         </div>
                     </div>
@@ -312,7 +223,28 @@
                             </span>
                         </button>
                     </div>
-                </div>
+                </div> -->
+                <div class="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
+    <form id="messageForm" class="flex-grow ml-4">
+        <div class="relative w-full">
+            <input type="text" name="messageInput" class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10" />
+            <button type="submit" class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+                <!-- You can add an icon or content inside the button if needed -->
+            </button>
+        </div>
+    </form>
+    <div class="ml-4">
+        <button type="button" onclick="sendMessage()" class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+            <span>Send</span>
+            <span class="ml-2">
+                <svg class="w-4 h-4 transform rotate-45 -mt-px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+                </svg>
+            </span>
+        </button>
+    </div>
+</div>
+
             </div>
         </div>
     </div>
